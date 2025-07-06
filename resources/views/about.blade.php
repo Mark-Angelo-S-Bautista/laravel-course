58bbd8d0da0304518c62b5a149530d2249818d00
Mark Angelo Bautista <179707263+Mark-Angelo-S-Bautista@users.noreply.github.com>
2025-07-06 14:49:09 +0800

    Routing

---
+++ resources/views/about.blade.php
@@ -0,0 +1,4 @@
+<h1>Hello Welcome to my Laravel Course</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus repellat molestiae, doloremque accusantium quisquam
+    unde esse dicta, sit provident cum laborum dignissimos dolorem obcaecati maxime quasi accusamus voluptatum culpa
+    velit.</p>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::view('/about', 'about');
+
+Route::get('/product/{id}', function(string $id){
+    return "Product ID: " . $id;
+})->whereNumber('id');
+
+Route::get('/sum/{num1}/{num2}', function(float $num1, $num2){
+    return $num1 + $num2;
+})->whereNumber('num1, num2');
