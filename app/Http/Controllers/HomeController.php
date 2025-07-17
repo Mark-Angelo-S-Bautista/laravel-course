4c57c9bbbe8c19c46d60620a2798aaf98c5d612b
Mark Angelo Bautista <179707263+Mark-Angelo-S-Bautista@users.noreply.github.com>
2025-07-17 22:29:00 +0800

    Selecting form Database 6:03:23

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Car;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
+        $cars = Car::get();
+        dump($cars);
         return view('home.index');
     }
 }
